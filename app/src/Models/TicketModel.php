120969548bd2bdeb394d7e9589cd202271c17f2f
Guldali Nergis <730220@student.inholland.nl>
2026-02-16 19:48:26 +0100

    ticketmodel created

---
+++ app/src/Models/TicketModel.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\Models\EventModel;
+use App\Models\UserModel;
+
+class TicketModel
+{
+    private int $id;
+    private EventModel $event;
+    private UserModel $user;
+    private int $numberOfPeople;
+
+    public function __construct(
+        int $id,
+        EventModel $event,
+        UserModel $user,
+        int $numberOfPeople
+    ) {
+        $this->id = $id;
+        $this->event = $event;
+        $this->user = $user;
+        $this->setNumberOfPeople($numberOfPeople);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getEvent(): EventModel
+    {
+        return $this->event;
+    }
+
+    public function getUser(): UserModel
+    {
+        return $this->user;
+    }
+
+    public function getNumberOfPeople(): int
+    {
+        return $this->numberOfPeople;
+    }
+
+    // public function getPrice(): float
+    // {
+    //     return $this->numberOfPeople * $this->event->getPrice();
+    // }
+
+    public function setNumberOfPeople(int $numberOfPeople): void
+    {
+        $this->numberOfPeople = $numberOfPeople;
+    }
+
+    public function setEvent(EventModel $event): void
+    {
+        $this->event = $event;
+    }
+
+    public function setUser(UserModel $user): void
+    {
+        $this->user = $user;
+    }
+
+   
+}

e39d526bb11e1f7e47d92c598db45dfa1972debf
Mahmoud Hashish <665241@student.inholland.nl>
2026-02-19 02:14:11 +0100

    home page view+header+footer

--- app/public/index.php
+++ app/public/index.php
@@ -9,7 +9,8 @@ use function FastRoute\simpleDispatcher;
 
 $dispatcher = simpleDispatcher(function (RouteCollector $r) {
     //$r->addRoute('GET', '/', ['App\Controllers\HomeController', 'home']);
-    $r->addRoute('GET', '/', ['App\Controllers\AuthController', 'index']);
+ $r->addRoute('GET', '/', ['App\Controllers\HomeController', 'index']);
+
 
     $r->addRoute('GET',  '/login', ['App\Controllers\AuthController', 'showLoginForm']);
     $r->addRoute('POST', '/login', ['App\Controllers\AuthController', 'login']);

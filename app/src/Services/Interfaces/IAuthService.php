1f77ca59b487e830ff2ae1c971694d4116a67cf0
Maniarmi <730336@student.inholland.nl>
2026-02-19 12:21:42 +0100

    error handling in controller+ redirection after logging in

---
+++ app/src/Services/Interfaces/IAuthService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Models\UserModel;
+
+interface IAuthService
+{
+    
+    public function emailExists(string $email): bool;
+    public function userNameExists(string $userName): bool;
+    public function createUser(UserModel $user): bool;
+    public function getUserByEmail(string $email): ?array;
+    public function logout(): void;
+}

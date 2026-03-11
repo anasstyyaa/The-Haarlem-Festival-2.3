dd6f5a30f1fd3aa8a6c5d65066f379fb45fa5efa
Mahmoud Hashish <665241@student.inholland.nl>
2026-03-11 23:36:44 +0100

    fixed route issue after merging branches

--- app/public/index.php
+++ app/public/index.php
@@ -7,26 +7,28 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// ini_set('display_errors', '0'); // hide from browser
+// ini_set('log_errors', '1');     // log instead
+
 use FastRoute\RouteCollector;
 use App\Models\Enums\EventTypeEnum;
 use function FastRoute\simpleDispatcher;
 
 $dispatcher = simpleDispatcher(function (RouteCollector $r) {
-
     //$r->addRoute('GET', '/', ['App\Controllers\HomeController', 'home']);
     $r->addRoute('GET', '/', ['App\Controllers\AuthController', 'index']);
 
-    $r->addRoute('GET',  '/login', ['App\Controllers\AuthController', 'showLoginForm']);
+    $r->addRoute('GET', '/login', ['App\Controllers\AuthController', 'showLoginForm']);
     $r->addRoute('POST', '/login', ['App\Controllers\AuthController', 'login']);
-    $r->addRoute('GET',  '/logout', ['App\Controllers\AuthController', 'logout']);
+    $r->addRoute('GET', '/logout', ['App\Controllers\AuthController', 'logout']);
 
     // Admin User Management
     $r->addRoute('GET', '/admin/users', ['App\Controllers\UserController', 'index']);
     $r->addRoute('POST', '/admin/users/delete', ['App\Controllers\UserController', 'delete']);
-    $r->addRoute('GET',  '/admin/users/create', ['App\Controllers\UserController', 'create']);
+    $r->addRoute('GET', '/admin/users/create', ['App\Controllers\UserController', 'create']);
     $r->addRoute('POST', '/admin/users/create', ['App\Controllers\UserController', 'create']);
-    $r->addRoute('GET',  '/admin/users/edit',   ['App\Controllers\UserController', 'edit']);
-    $r->addRoute('POST', '/admin/users/edit',   ['App\Controllers\UserController', 'edit']);
+    $r->addRoute('GET', '/admin/users/edit', ['App\Controllers\UserController', 'edit']);
+    $r->addRoute('POST', '/admin/users/edit', ['App\Controllers\UserController', 'edit']);
     $r->addRoute('POST', '/admin/users/restore', ['App\Controllers\UserController', 'restore']);
 
     // Admin Restaurant Management
@@ -60,26 +62,28 @@ $dispatcher = simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('GET', '/jazz/{id:\d+}', ['App\Controllers\JazzController', 'detail']);
 
     // Admin Chef Management
-    $r->addRoute('GET',  '/admin/chefs/create', ['App\Controllers\ChefController', 'showCreateForm']);
+    $r->addRoute('GET', '/admin/chefs/create', ['App\Controllers\ChefController', 'showCreateForm']);
     $r->addRoute('POST', '/admin/chefs/create', ['App\Controllers\ChefController', 'store']);
-    $r->addRoute('GET',  '/admin/chefs/edit/{id:\d+}', ['App\Controllers\ChefController', 'showEditForm']);
+    $r->addRoute('GET', '/admin/chefs/edit/{id:\d+}', ['App\Controllers\ChefController', 'showEditForm']);
     $r->addRoute('POST', '/admin/chefs/edit/{id:\d+}', ['App\Controllers\ChefController', 'update']);
-    $r->addRoute('GET',  '/admin/chefs/delete/{id:\d+}', ['App\Controllers\ChefController', 'delete']);
+    $r->addRoute('GET', '/admin/chefs/delete/{id:\d+}', ['App\Controllers\ChefController', 'delete']);
 
-    $r->addRoute('GET',  '/register', ['App\Controllers\AuthController', 'showRegisterForm']);
+    $r->addRoute('GET', '/register', ['App\Controllers\AuthController', 'showRegisterForm']);
     $r->addRoute('POST', '/register', ['App\Controllers\AuthController', 'register']);
 
     $r->addRoute('POST', '/addTicket', ['App\Controllers\TicketController', 'addTicket']);
-    $r->addRoute('GET',  '/kidsEvent', ['App\Controllers\KidsEventController', 'index']);
-    $r->addRoute('GET',  '/personalProgram', ['App\Controllers\TicketController', 'index']);
-    $r->addRoute('GET',  '/admin/kidsPage', ['App\Controllers\KidsEventController', 'adminIndex']);
-    $r->addRoute('GET',  '/admin/elements/edit/{id:\d+}', ['App\Controllers\PageElementController', 'showEditForm']);
+    $r->addRoute('GET', '/kidsEvent', ['App\Controllers\KidsEventController', 'index']);
+    $r->addRoute('GET', '/personalProgram', ['App\Controllers\TicketController', 'index']);
+    $r->addRoute('GET', '/admin/kidsPage', ['App\Controllers\KidsEventController', 'adminIndex']);
+    $r->addRoute('GET', '/admin/elements/edit/{id:\d+}', ['App\Controllers\PageElementController', 'showEditForm']);
 
     // Yummy / Restaurant Routes
     $r->addRoute('GET', '/yummy', ['App\Controllers\RestaurantController', 'index']);
     $r->addRoute('GET', '/yummy/restaurant/{id:\d+}', ['App\Controllers\RestaurantController', 'showDetails']);
 
     // History routes
+    $r->addRoute('GET', '/history', ['App\Controllers\HistoryController', 'index']);
+    $r->addRoute('POST', '/history/book', ['App\Controllers\HistoryController', 'book']);
     $r->addRoute('GET', '/admin/history/venues', ['App\Controllers\HistoryController', 'adminVenues']);
     $r->addRoute('GET', '/admin/history/venues/create', ['App\Controllers\HistoryController', 'createVenue']);
     $r->addRoute('POST', '/admin/history/venues/create', ['App\Controllers\HistoryController', 'storeVenue']);
@@ -87,4 +91,64 @@ $dispatcher = simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('POST', '/admin/history/venues/edit', ['App\Controllers\HistoryController', 'updateVenue']);
     $r->addRoute('POST', '/admin/history/venues/delete', ['App\Controllers\HistoryController', 'deleteVenue']);
 
+    // Yummy / Reservation
 });
+
+/**
+ * Get the request method and URI from the server variables and invoke the dispatcher.
+ */
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+/**
+ * Switch on the dispatcher result and call the appropriate controller method if found.
+ */
+switch ($routeInfo[0]) {
+    // Handle not found routes
+    case FastRoute\Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        echo 'Not Found';
+        break;
+
+    // Handle routes that were invoked with the wrong HTTP method
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        http_response_code(405);
+        echo 'Method Not Allowed';
+        break;
+
+    // Handle found routes
+    case FastRoute\Dispatcher::FOUND:
+        [$class, $method] = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        if ($class === 'App\Controllers\UserController') {
+            $repository = new \App\Repositories\UserRepository();
+            $service = new \App\Services\UserService($repository);
+            $authService = new \App\Services\AuthService($repository);
+            $controller = new $class($service, $authService);
+        } elseif ($class === 'App\Controllers\RestaurantController') {
+            $repository = new \App\Repositories\Yummy\RestaurantRepository();
+            $chefRepo = new \App\Repositories\Yummy\ChefRepository();
+            $sessionRepo = new \App\Repositories\Yummy\RestaurantSessionRepository();
+            $service = new \App\Services\Yummy\RestaurantService($repository);
+            $chefService = new \App\Services\Yummy\ChefService($chefRepo);
+            $sessionService = new \App\Services\Yummy\RestaurantSessionService($sessionRepo, $repository);
+            $controller = new $class($service, $chefService, $sessionService);
+        } elseif ($class === 'App\Controllers\JazzController') {
+            $artistRepository = new \App\Repositories\ArtistRepository();
+            $artistService = new \App\Services\ArtistService($artistRepository);
+            $jazzEventRepository = new \App\Repositories\JazzEventRepository();
+            $jazzEventService = new \App\Services\JazzEventService($jazzEventRepository);
+            $controller = new $class($artistService, $jazzEventService);
+        } elseif ($class === 'App\Controllers\ChefController') {
+            $chefRepo = new \App\Repositories\Yummy\ChefRepository();
+            $chefService = new \App\Services\Yummy\ChefService($chefRepo);
+            $controller = new $class($chefService);
+        } else {
+            $controller = new $class();
+        }
+
+        echo $controller->$method($vars);
+        break;
+}
